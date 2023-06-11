e829850a3c6e86d2540e1a0f27d22daf459889d7
orrellij <ianjorrell12@gmail.com>
2023-06-11 19:11:18 -0400

    3rd commit: finishined code 	modified:   py-introduction-to-sets.py

--- 011lpy-introduction-to-sets/code/py-introduction-to-sets.py
+++ 011lpy-introduction-to-sets/code/py-introduction-to-sets.py
@@ -1,6 +1,13 @@
 def average(array):
     uniqueHts = set(array)
     # here I turn the list of heights into a unique one using set()
+    
+    numOfDistHts = len(uniqueHts)
+    # the above line obtains the number of distinct heights in the set
+    # using len() allows me to obtain this data.
+    
+    avg = sum(uniqueHts,0) / numOfDistHts
+    return avg
 
 if __name__ == '__main__':
     n = int(input())
