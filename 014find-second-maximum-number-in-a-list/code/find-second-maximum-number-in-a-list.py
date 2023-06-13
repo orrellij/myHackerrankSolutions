ae0f71a717483d2db58e9f4ea0253e034c9c95a2
orrellij <ianjorrell12@gmail.com>
2023-06-13 13:16:18 -0400

    2nd Commit: making a set 	modified:   find-second-maximum-number-in-a-list.py 	deleted:    ../../011lpy-introduction-to-sets/code/py-introduction-to-sets.py 	../../011py-introduction-to-sets/ 	../../012finding-the-percentage/code/finding-the-percentage.txt

--- 014find-second-maximum-number-in-a-list/code/find-second-maximum-number-in-a-list.py
+++ 014find-second-maximum-number-in-a-list/code/find-second-maximum-number-in-a-list.py
@@ -2,3 +2,12 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
+    
+    # first I turn the inputted numbers that were made into a list
+    # into a set so it has no repeating numbers. This makes my work
+    # easier
+    no_repeats = set(arr)
+    
+    
+           
+    
