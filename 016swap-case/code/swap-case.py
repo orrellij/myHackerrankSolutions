c1d7e7a36167d225c913788a7737f1e398ac5597
orrellij <orrellij@appstate.edu>
2023-06-17 17:04:11 -0400

    Third Commit 	modified:   swap-case.py 	../../015np-arrays/code/np-arrays.txt

--- 016swap-case/code/swap-case.py
+++ 016swap-case/code/swap-case.py
@@ -1,5 +1,6 @@
 def swap_case(s):
-    return s.swapcase()
+    return s.swapcase() # this is the final code. Using the swapcase fuction from the String class
+                        # it was easy to swap the lowers to uppers and vice versa.
 
 if __name__ == '__main__':
     s = input()
