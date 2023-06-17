ce3dfd857b0535f1897bc54b0e53f7afca1a5f40
orrellij <orrellij@appstate.edu>
2023-06-17 16:22:59 -0400

    Third Commit 	modified:   np-arrays.py

--- 015np-arrays/code/np-arrays.py
+++ 015np-arrays/code/np-arrays.py
@@ -2,6 +2,8 @@ import numpy
 
 def arrays(arr):
     my_array = numpy.array(arr, float) #this creates an array from the list created in input
+    sorted_array = numpy.flip(my_array) #uses the flip function in numpy to obtain the array we want
+    return sorted_array
 
 arr = input().strip().split(' ')
 result = arrays(arr)
