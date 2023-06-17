4926b9677e27997aaeb36e3a37828cfc38dd3363
orrellij <orrellij@appstate.edu>
2023-06-17 16:20:50 -0400

    Second Commit 	modified:   np-arrays.py

--- 015np-arrays/code/np-arrays.py
+++ 015np-arrays/code/np-arrays.py
@@ -1,7 +1,7 @@
 import numpy
 
 def arrays(arr):
-    
+    my_array = numpy.array(arr, float) #this creates an array from the list created in input
 
 arr = input().strip().split(' ')
 result = arrays(arr)
