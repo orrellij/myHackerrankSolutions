d218bbd8e06302267fe7a535f21bb732b841e9e4
orrellij <orrellij@appstate.edu>
2023-06-20 02:41:22 -0400

    Third Commit: final 	modified:   python-string-split-and-join.py 	../../015np-arrays/code/np-arrays.txt 	../../016swap-case/code/swap-case.txt

--- 017python-string-split-and-join/code/python-string-split-and-join.py
+++ 017python-string-split-and-join/code/python-string-split-and-join.py
@@ -1,9 +1,9 @@
 def split_and_join(line):
-    split = line.split(" ")
-    joined = "-".join(split)
+    split = line.split(" ") #splits the string up by spaces
+    joined = "-".join(split) #joins the strings split up connecting them with hyphens
     return joined
 
-if __name__ == '__main__':
+if __name__ == '__main__': #code stub given by Hackerrank
     line = input()
     result = split_and_join(line)
     print(result)
